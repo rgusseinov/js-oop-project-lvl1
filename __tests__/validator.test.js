d49e6dfc3514b968581f6e269bab458c3dc03667
Ruslan Gusseinov <r.gusseinov@mail.ru>
2022-07-09 11:45:08 +0600

    feat: add git ignore and test cases

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -4,17 +4,19 @@
 import { test, expect } from "@jest/globals";
 import Validator from "../src/Validator.js";
 
-const v = new Validator();
-const schema = v.string();
-
 test("Validator without required", () => {
+  const v = new Validator();
+  const schema = v.string();
   expect(schema.isValid(123)).toBe(false);
   expect(schema.isValid()).toBe(true);
+  expect(schema.isValid("Almaty")).toBe(true);
   expect(schema.isValid(null)).toBe(true);
   expect(schema.isValid(undefined)).toBe(true);
 });
 
 test("Validator with required", () => {
+  const v = new Validator();
+  const schema = v.string();
   schema.required();
   expect(schema.isValid("what does the fox say")).toBe(true);
   expect(schema.isValid("hexlet")).toBe(true);
@@ -23,10 +25,20 @@ test("Validator with required", () => {
 });
 
 test("Validator contains", () => {
+  const v = new Validator();
+  const schema = v.string();
   schema.required();
   expect(schema.contains("what").isValid("what does the fox say")).toBe(true);
   expect(schema.contains("whatthe").isValid("what does the fox say")).toBe(
     false
   );
   expect(schema.isValid("what does the fox say")).toBe(false);
+});
+
+test("Validator with minLength", () => {
+  const v = new Validator();
+  const schema = v.string();
+  schema.minLength(5);
+  expect(schema.isValid("Hello from Almaty")).toBe(true);
+  expect(schema.isValid("ALA")).toBe(false);
 });
