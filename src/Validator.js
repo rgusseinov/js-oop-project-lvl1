ab82fc1574a39f7269d05c2ea7c9f8642a5d9402
Ruslan Gusseinov <r.gusseinov@mail.ru>
2022-07-16 16:36:08 +0600

    refactor: fix issue with hasOwnProperty #2

--- src/Validator.js
+++ src/Validator.js
@@ -8,7 +8,7 @@ export default class Validator {
   }
 
   addValidator(type, name, fn) {
-    if (!Object.prototype.hasOwnProperty(type)) {
+    if (!['string', 'number', 'array', 'object'].includes(type)) {
       throw new Error(`Method ${type} not exist`);
     }
     if (!this.assignValidators[type]) {
