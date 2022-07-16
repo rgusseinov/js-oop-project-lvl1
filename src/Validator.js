f71fdf148875aa5b48b549b354a6a2554e093642
Ruslan Gusseinov <r.gusseinov@mail.ru>
2022-07-16 17:12:54 +0600

    refactor: fix issue with hasOwnProperty #10

--- src/Validator.js
+++ src/Validator.js
@@ -4,7 +4,7 @@ import {
 
 export default class Validator {
   constructor() {
-    this.assignValidators = {};
+    this.assignValidators = {string: {}, number: {}, array: {}, object: {}};
   }
 
   addValidator(type, name, fn) {
